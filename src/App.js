6aeada7775c8f89f878703067552252a24a55ec8
GwonWooL <229861@naver.com>
2025-10-19 16:38:16 +0900

    개선 코드

--- src/App.js
+++ src/App.js
@@ -2,96 +2,70 @@ import fs from "fs";
 
 class App {
   async run() {
-    let str = fs.readFileSync(0, "utf-8").trim().split("");
-    let arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    let sum = 0
-    let point = 0
 
-    function isblanked() {
-      if(str[0]!='"'&&str[1]!='"') {
-        return true
-      }
-      console.log(0)
-    }
-    let cus = "" //구분자가 한개 나온다는 가정
-    function isCustom() {
-        for(let i = 2; i<str.length-1; i++) {
+    let str = fs.readFileSync(0, "utf-8").trim().split("");
+    let divider = [',', ':', '-']
+
+    let point = 0
+    let custom = false
+
+    for(let i = 2; i<str.length-2; i++) {
         if(str[i+1]=='\\'&&str[i+2]=='n'&&str[i-2]=='/' && str[i-1]=='/' )  {
-            cus += str[i]
+            divider.push(str[i])
             point = i
-            return true   
-            }
+            custom = true
+        }
+    }
+
+    function isblanked(str) {
+        if(str.length==0) {
+            return true
         }
         return false
     }
 
-    function customCalcul() {
+    function calculate(str, startIndex, divider) {
+        let sum = 0
         let num = ""
-        for(let i = point+3; i<str.length; i++) {
-            let isExit = arr.includes(str[i])
-            if(isExit==true) {
-                num += str[i]
-                if(i==str.length-1) {
-                    sum += Number(num)
+        let error = false
+        for(let i = startIndex; i<str.length; i++) {
+            let isExist = Number.isInteger(Number(str[i]))
+            if(isExist) {
+                if(str[i-1]=='-') {
+                    num+='-'
                 }
-                if(str[i+1]==','||str[i+1]==':'||str[i+1]==cus) { //구분자가 한 개일 경우에만 작동
+                num+=str[i]
+                if(i==str.length-1||divider.includes(str[i+1])) {
                     sum += Number(num)
                     num = ""
                 }
             }
-            if(isExit==false) {
-                if(str[i]!=',' && str[i]!=':' && str[i]!=cus) {
-                    return false
-                }
-            } 
-        }
-        return true
-    }
-
-    function noncustomCalcul() {
-        let num = ""
-        for(let i = 0; i<str.length; i++) {
-            let isExit = arr.includes(str[i])
-            if(isExit==true) {
-                num += str[i]
-                if(i==str.length-1) {
-                    sum += Number(num)
-                }
-                if(str[i+1]==','||str[i+1]==':') { //구분자가 한 개일 경우에만 작동
-                    sum += Number(num)
-                    num = ""
+            else {
+                if(!divider.includes(str[i])) {
+                    error = true
+                    return {error: true}
                 }
             }
-            if(isExit==false&&((str[i]!=','&&str[i]!=':'))) {
-                return false
-            }      
         }
-        return true
+        return {error: false, sum}
     }
 
-   
-    if(isblanked() ) {
-      if(isCustom()) {
-          if(!customCalcul()) {
+    if(isblanked(str)) {
+        console.log("0")
+    }
+    else {
+        let startIndex = custom ? point+3 : 0
+        let result = calculate(str, startIndex, divider)
+
+        if(result.error) {
             console.log("ERROR")
-          }
-          else {
-            console.log("덧셈할 문자열을 입력해 주세요.")
-            console.log(str.join(""))
-            console.log(sum)
-          }
-      }
-      else {
-          if(!noncustomCalcul()) {
-            console.log("ERROR")
-          }
-          else {
-            console.log("덧셈할 문자열을 입력해 주세요.")
-            console.log(str.join(""))
-            console.log(sum)
-          } 
         }
-      }
+        else {
+            console.log("덧셈할 문자열을 입력해 주세요.")
+            console.log(str.join(""))
+            console.log(result.sum)
+        }
+    }
   }
 }
 
