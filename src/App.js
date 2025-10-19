5d052622645c4b4b56296ecf486b021162c8323e
GwonWooL <229861@naver.com>
2025-10-19 18:13:57 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,71 @@
-import fs from "fs";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {
+    async run() {
+        const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요."); 
+        let str = input.trim().split("");
+        let divider = [',', ':', '-']
 
-    let str = fs.readFileSync(0, "utf-8").trim().split("");
-    let divider = [',', ':', '-']
+        let point = 0
+        let custom = false
 
-    let point = 0
-    let custom = false
-
-    for(let i = 2; i<str.length-2; i++) {
-        if(str[i+1]=='\\'&&str[i+2]=='n'&&str[i-2]=='/' && str[i-1]=='/' )  {
-            divider.push(str[i])
-            point = i
-            custom = true
-        }
-    }
-
-    function isblanked(str) {
-        if(str.length==0) {
-            return true
-        }
-        return false
-    }
-
-    function calculate(str, startIndex, divider) {
-        let sum = 0
-        let num = ""
-        let error = false
-        for(let i = startIndex; i<str.length; i++) {
-            let isExist = Number.isInteger(Number(str[i]))
-            if(isExist) {
-                if(str[i-1]=='-') {
-                    num+='-'
-                }
-                num+=str[i]
-                if(i==str.length-1||divider.includes(str[i+1])) {
-                    sum += Number(num)
-                    num = ""
-                }
-            }
-            else {
-                if(!divider.includes(str[i])) {
-                    error = true
-                    return {error: true}
-                }
+        for(let i = 2; i<str.length-2; i++) {
+            if(str[i+1]=='\\'&&str[i+2]=='n'&&str[i-2]=='/' && str[i-1]=='/' )  {
+                divider.push(str[i])
+                point = i
+                custom = true
             }
         }
-        return {error: false, sum}
-    }
 
-    if(isblanked(str)) {
-        console.log("0")
-    }
-    else {
-        let startIndex = custom ? point+3 : 0
-        let result = calculate(str, startIndex, divider)
+        function isblanked(str) {
+            if(str.length==0) {
+                return true
+            }
+            return false
+        }
 
-        if(result.error) {
-            console.log("ERROR")
+        function calculate(str, startIndex, divider) {
+            let sum = 0
+            let num = ""
+            let error = false
+            for(let i = startIndex; i<str.length; i++) {
+                let isExist = Number.isInteger(Number(str[i]))
+                if(isExist) {
+                    if(str[i-1]=='-') {
+                        num+='-'
+                    }
+                    num+=str[i]
+                    if(i==str.length-1||divider.includes(str[i+1])) {
+                        sum += Number(num)
+                        num = ""
+                    }
+                }
+                else {
+                    if(!divider.includes(str[i])) {
+                        error = true
+                        return {error: true}
+                    }
+                }
+            }
+            return {error: false, sum}
+        }
+
+        if(isblanked(str)) {
+            Console.print("0")
         }
         else {
-            console.log("덧셈할 문자열을 입력해 주세요.")
-            console.log(str.join(""))
-            console.log(result.sum)
-        }
-    }
-  }
-}
+            let startIndex = custom ? point+3 : 0
+            let result = calculate(str, startIndex, divider)
 
+            if(result.error) {
+                Console.print("ERROR")
+            }
+            else {
+                Console.print(str.join(""))
+                Console.print(result.sum)
+            }
+        }
+
+    }
+}
 export default App;
